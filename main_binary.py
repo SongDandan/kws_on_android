731f699617731396cbb874bf1f22f0504cf7d5d2
hgcdanniel <573705965@qq.com>
2024-05-29 10:52:56 +0800

    add insert

--- main_binary.py
+++ main_binary.py
@@ -19,3 +19,22 @@ if index != -1:
     print(f"元素 {x} 的索引为 {index}")
 else:
     print(f"数组中不存在 {x}")
+
+
+def insertion_sort(arr):
+    for i in range(len(arr)):
+        # 获取当前需要排序的元素
+        key = arr[i]
+        # 从已排序的序列的末尾开始比较，并找到插入点
+        j = i - 1
+        while j >= 0 and key < arr[j]:
+            # 如果发现有元素比当前元素小，则交换它们
+            arr[j+1] = arr[j]
+            arr[j] = key
+            j -= 1
+    return arr
+ 
+# 示例使用
+arr = [12, 11, 13, 5, 6]
+sorted_arr = insertion_sort(arr)
+print("Sorted array:", sorted_arr)
