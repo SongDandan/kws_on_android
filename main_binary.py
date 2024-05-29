8d85de2e40fec312ef0d1a04358c8f42ef3eb539
hgcdanniel <573705965@qq.com>
2024-05-29 11:42:13 +0800

    add bubble

--- main_binary.py
+++ main_binary.py
@@ -1,3 +1,17 @@
+def bubble_sort(arr):
+    n = len(arr)
+    for i in range(n):
+        for j in range(0, n-i-1):
+            if arr[j] > arr[j+1]:
+                arr[j], arr[j+1] = arr[j+1], arr[j]
+    return arr
+ 
+# 示例使用
+array = [64, 34, 25, 12, 22, 11, 90]
+sorted_array = bubble_sort(array)
+print("Sorted array is:", sorted_array)
+
+
 def binary_search(arr, x):
     low = 0
     high = len(arr) - 1
